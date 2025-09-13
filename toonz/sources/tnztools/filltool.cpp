18fafe5c0786cbb10f229466a3b884dc86e7fd4a
Abel You <abelyou032@gmail.com>
2025-09-13 18:50:29 +0800

    Add lock after getting the CMraster to fill

--- toonz/sources/tnztools/filltool.cpp
+++ toonz/sources/tnztools/filltool.cpp
@@ -2071,6 +2071,7 @@ void FillTool::computeRefImgsIfNeeded(const FillParameters &params) {
     if (!ti) continue;
 
     auto raux             = ti->getRaster();
+    raux->lock();
     auto imgId            = sl->getImageId(fid, 0);
     TPointD saveboxOffset = TPointD(0, 0);
     if (fillOnlySavebox) {
@@ -2090,6 +2091,8 @@ void FillTool::computeRefImgsIfNeeded(const FillParameters &params) {
       drawReferImage(ras, xsh, m_beginCell.col, slFidToRow[{sl, fid}],
                      saveboxOffset);
     if (closeGap) gapClose(ras, raux, sl, referFill);
+    
+    raux->unlock();
   }
 }
 
