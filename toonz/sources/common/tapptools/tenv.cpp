4cfd98b2c6169270d8ba593eb0503769c5fb5c91
shun-iwasawa <shun.iwasawa@ponoc.jp>
2025-10-22 13:10:33 +0900

    fix registering multiple system var paths

--- toonz/sources/common/tapptools/tenv.cpp
+++ toonz/sources/common/tapptools/tenv.cpp
@@ -274,7 +274,7 @@ public:
   }
 
   void setArgPathValue(std::string key, std::string value) {
-    m_argPathValues.emplace(key, value);
+    m_argPathValues.insert_or_assign(key, value);
     if (key == m_systemVarPrefix + "PROFILES") updateEnvFile();
   }
 
